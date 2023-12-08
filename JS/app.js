6660d422a0215d9b92a739f10e2e0bd42f9efb5f
Alejandro Agudelo <alejo.agudelo.serna@gmail.com>
2023-12-07 23:53:24 -0500

    add login

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,11 @@
 function calcularIMC(){
+    document.getElementById("resultadoIMC").innerHTML="";
     let peso = parseFloat(document.getElementById('peso').value);
     let altura = parseFloat(document.getElementById('altura').value);
-    let pesoIdeal = peso/(altura*altura).toFixed(2);
+    let pesoIdeal = peso/(altura*altura);
     document.getElementById("resultadoIMC").innerHTML=document.getElementById("resultadoIMC").innerHTML+
                     `<div class="col bg-body-tertiary">
-                        <h3>Tu IMC es</h3><p>${pesoIdeal} kg</p>
+                        <h3>Tu IMC es</h3><p>${pesoIdeal.toFixed(2)} kg</p>
                     </div`;
 }
+
